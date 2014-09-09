f181fa31c8302ef61cc7b0d3c90f8716cb6825e1
chethan <che4on@gmail.com>
2014-09-09 10:49:27 +0530

    data.registered_users is parsed.

--- js/load.js
+++ js/load.js
@@ -3,9 +3,9 @@ function getCount()
 {
 	$.get( "http://gaadikey.in/getCount", function( data ) {
 
-	   console.log("No of users is "+data.length);
+	   console.log("No of users is "+data.registered_users);
 	  $( "page-header" )
-	    .append( "<small>Over "+data.length+"+ registered users.</small>") //2+
+	    .append( "<small>Over "+data.registered_users+"+ registered users.</small>") //2+
 	}, "json" ); 
 
 }
