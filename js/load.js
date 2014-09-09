e1731504b78ebd778387d2f1445a76b4f82d486e
chethan <che4on@gmail.com>
2014-09-09 10:44:47 +0530

    Calling getCount() when body onload.

--- js/load.js
+++ js/load.js
@@ -1,6 +1,11 @@
-$.get( "http://gaadikey.in/getCount", function( data ) {
 
-   console.log("No of users is "+data.length);
-  $( "page-header" )
-    .append( "<small>Over "+data.length+"+ registered users.</small>") //2+
-}, "json" ); 
+function getCount()
+{
+	$.get( "http://gaadikey.in/getCount", function( data ) {
+
+	   console.log("No of users is "+data.length);
+	  $( "page-header" )
+	    .append( "<small>Over "+data.length+"+ registered users.</small>") //2+
+	}, "json" ); 
+
+}
