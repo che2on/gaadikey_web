ab28c4d5d38cbec3009eab4fb2588f86c124a5a6
chethan <che4on@gmail.com>
2014-09-09 10:53:57 +0530

    Consoling the whole data object

--- js/load.js
+++ js/load.js
@@ -2,7 +2,7 @@
 function getCount()
 {
 	$.get( "http://gaadikey.in/getCount", function( data ) {
-
+		console.log("Data is "+data);
 	   console.log("No of users is "+data.registered_users);
 	  $( "page-header" )
 	    .append( "<small>Over "+data.registered_users+"+ registered users.</small>") //2+
