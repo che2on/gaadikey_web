8f459022fbaa82120cd1acd7a9dce3d9cfb1327c
chethan <che4on@gmail.com>
2014-09-14 23:37:41 +0530

    pinging the right url to get the count of registered users

--- js/load.js
+++ js/load.js
@@ -1,7 +1,7 @@
 
 function getCount()
 {
-	$.get( "http://gaadikey.in/getCount", function( data ) {
+	$.get( "https://gaadikey.in/getCount", function( data ) {
 		console.log("Data is "+data);
 	   console.log("No of users is "+data.registered_users);
 	  $( ".page-header" )
