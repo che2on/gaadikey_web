f13d2ae8609dbee2832a2525189e6f89488f2724
chethan <che4on@gmail.com>
2014-11-11 14:38:45 +0530

    Reach Count is now retreived!

--- js/load.js
+++ js/load.js
@@ -8,4 +8,14 @@ function getCount()
 	    .append( "<small>Over "+data.registered_users+"+ registered users.</small>") //2+
 	}, "json" ); 
 
+
+	$.get( "https://gaadikey.in/reach", function( data ) {
+		console.log("Data is "+data);
+	   console.log("Reach is "+data.reach);
+	  $( ".page-header" )
+	    .append( "<small>Reaching around "+data.reach+"+ people.</small>") //2+
+	}, "json" );
+
+
+
 }
